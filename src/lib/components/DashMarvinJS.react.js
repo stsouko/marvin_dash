650a7c08ad8b7555127b51373adfc60028d34447
Ramil Nugmanov <stsouko@live.ru>
2019-10-09 23:25:22 +0300

    python>js load implemented

--- src/lib/components/DashMarvinJS.react.js
+++ src/lib/components/DashMarvinJS.react.js
@@ -3,37 +3,36 @@ import PropTypes from 'prop-types';
 
 
 export default class DashMarvinJS extends Component {
-    render() {
-        const {id, marvin_url, marvin_width, marvin_height, setProps} = this.props;
+    componentDidUpdate(prevProps) {
+        if (this.props.upload !== prevProps.upload) {
+            this.render_upload(this.props.upload);
+        }
+    }
 
-        function marvin_events() {
-            const sketcher = this.marvin.sketcherInstance;
-            const button = {
-                'name' : 'Upload',
-                'image-url' : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAMAAAD04JH5AAAArlBMVEUAAAAAAAA' +
-                              'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
-                              'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
-                              'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeyFOlAAAAAXRSTlMAQObYZgAAAAFiS0' +
-                              'dEAIgFHUgAAAAJcEhZcwAACcUAAAnFAYeExPQAAAAHdElNRQfjCQwVADLJ+C5eAAABUklEQVR42u3ZQWrDMBC' +
-                              'F4TlMIFun2JD//hdLodBFLamWrMlL2jc7WWbmi2LkEY44FRsQulj4ClV9vkNdXyMArQC0AtAKQCsArQC0AtAK' +
-                              'QCsArQC0AtAKQCsArQC0AtAKQCsArWBfojpIESz7AvVRhqCQvjGcD9gK2Vvj6YJS7uaFXECtLX8WoH4uSHwIC' +
-                              'olpX1vTj2HFX5u3Eezzlpc7cTP+ua61/zth/V/zcGqAAQZkAY63ea25bbRZ7Ok06zMfzSzXevlLV69bnViGW2' +
-                              'bmAH5Ncxvv/I8AtuFTwyTAgTT3KUfvEwDeFrDOeQjHASEGHN2IkgBrx1Z8GpDxMjLAAAMMMMAAAwwwwAADDDD' +
-                              'AAAMMMODvAaLzO3EmIDSAGLt7IqDvO3EKoCcMMMAAAwwwwAADDDDAgPcCZIcBBrwmIP4RoLJTXNWAUNePuInr' +
-                              'f8b92eUfelqXBAH/Tb4AAAAASUVORK5CYII=',
-                'toolbar' : 'N'
-            };
-            sketcher.addButton(button, async () => {
-                const mrv = await sketcher.exportStructure('mrv');
-                if (mrv !== '<cml><MDocument></MDocument></cml>') {
-                    setProps({structure: mrv});
-                }
-            });
+    render_upload(){
+        // ad-hoc
+    }
+
+    marvin_onready() {
+        const sketcher = this.marvin.sketcherInstance;
+
+        sketcher.addButton(this.component.props.marvin_button, async () => {
+            const mrv = await sketcher.exportStructure('mrv');
+            if (mrv !== '<cml><MDocument></MDocument></cml>') {
+                this.component.props.setProps({download: mrv});
+            }
+        });
+        this.component.render_upload = mrv => {
+            sketcher.importAsMrv(mrv);
         }
-        function marvin_ready(e) {
-            const marvin = e.target.contentWindow.marvin;
-            marvin.onReady(marvin_events.bind({marvin: marvin}));
-        }
+    }
+    marvin_onload(e) {
+        const marvin = e.target.contentWindow.marvin;
+        marvin.onReady(this.marvin_onready.bind({marvin: marvin, component: this}));
+    }
+
+    render() {
+        const {id, marvin_url, marvin_width, marvin_height} = this.props;
 
         return (
             <div id={id}>
@@ -42,7 +41,7 @@ export default class DashMarvinJS extends Component {
                         width={marvin_width}
                         height={marvin_height}
                         data-toolbars='reaction'
-                        onLoad={marvin_ready}
+                        onLoad={this.marvin_onload.bind(this)}
                 />
             </div>
         );
@@ -52,6 +51,21 @@ export default class DashMarvinJS extends Component {
 DashMarvinJS.defaultProps = {
     marvin_width: 900,
     marvin_height: 450,
+    marvin_button: {
+        'name' : 'Upload',
+        'image-url' : 'data:image/png;base64,iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAMAAAD04JH5AAAArlBMVEUAAAAAAAA' +
+                      'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
+                      'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA' +
+                      'AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAABeyFOlAAAAAXRSTlMAQObYZgAAAAFiS0' +
+                      'dEAIgFHUgAAAAJcEhZcwAACcUAAAnFAYeExPQAAAAHdElNRQfjCQwVADLJ+C5eAAABUklEQVR42u3ZQWrDMBC' +
+                      'F4TlMIFun2JD//hdLodBFLamWrMlL2jc7WWbmi2LkEY44FRsQulj4ClV9vkNdXyMArQC0AtAKQCsArQC0AtAK' +
+                      'QCsArQC0AtAKQCsArQC0AtAKQCsArWBfojpIESz7AvVRhqCQvjGcD9gK2Vvj6YJS7uaFXECtLX8WoH4uSHwIC' +
+                      'olpX1vTj2HFX5u3Eezzlpc7cTP+ua61/zth/V/zcGqAAQZkAY63ea25bbRZ7Ok06zMfzSzXevlLV69bnViGW2' +
+                      'bmAH5Ncxvv/I8AtuFTwyTAgTT3KUfvEwDeFrDOeQjHASEGHN2IkgBrx1Z8GpDxMjLAAAMMMMAAAwwwwAADDDD' +
+                      'AAAMMMODvAaLzO3EmIDSAGLt7IqDvO3EKoCcMMMAAAwwwwAADDDDAgPcCZIcBBrwmIP4RoLJTXNWAUNePuInr' +
+                      'f8b92eUfelqXBAH/Tb4AAAAASUVORK5CYII=',
+        'toolbar' : 'N'
+    }
 };
 
 DashMarvinJS.propTypes = {
@@ -64,16 +78,33 @@ DashMarvinJS.propTypes = {
      * A URL of MarvinJS iframe.
      */
     marvin_url: PropTypes.string.isRequired,
+
     /**
-     * Size of MarvinJS iframe.
+     * Width of MarvinJS iframe.
      */
     marvin_width: PropTypes.number,
+    /**
+     * Height of MarvinJS iframe.
+     */
     marvin_height: PropTypes.number,
 
     /**
-     * The value displayed in the input.
+     * Button config of MarvinJS iframe.
      */
-    structure: PropTypes.string,
+    marvin_button: PropTypes.shape({
+        'name' : PropTypes.string,
+        'image-url' : PropTypes.string,
+        'toolbar' : PropTypes.string
+    }),
+
+    /**
+     * Storage for structure from backend
+     */
+    upload: PropTypes.string,
+    /**
+     * Storage for structure to backend
+     */
+    download: PropTypes.string,
 
     /**
      * Dash-assigned callback that should be called to report property changes
--- usage.py
+++ usage.py
@@ -2,6 +2,8 @@ import dash_marvinjs
 import dash
 from dash.dependencies import Input, Output
 import dash_html_components as html
+from CGRtools.files import MRVRead, MRVWrite
+from io import StringIO, BytesIO
 
 app = dash.Dash(__name__)
 
@@ -10,14 +12,22 @@ app.layout = html.Div([
         id='input',
         marvin_url=app.get_asset_url('mjs/editor.html'),
         marvin_width=600
-    ),
-    html.Div(id='output')
+    )
 ])
 
 
-@app.callback(Output('output', 'children'), [Input('input', 'structure')])
+@app.callback(Output('input', 'upload'), [Input('input', 'download')])
 def display_output(value):
-    return 'You have entered {}'.format(value)
+    if value:
+        with BytesIO(value.encode()) as f, MRVRead(f) as i:
+            s = next(i)
+            s.standardize()
+            s.thiele()
+        with StringIO() as f:
+            with MRVWrite(f) as o:
+                o.write(s)
+            value = f.getvalue()
+    return value
 
 
 if __name__ == '__main__':
